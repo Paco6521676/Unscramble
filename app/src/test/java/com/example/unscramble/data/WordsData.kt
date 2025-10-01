8e5c43784a13dee917f944b75d416ef5bc64485a
Francisco Contreras <fcontreras06@alumnos.uaq.mx>
2025-09-30 20:46:52 -0600

    Unscramble

--- app/src/test/java/com/example/unscramble/data/WordsData.kt
+++ app/src/test/java/com/example/unscramble/data/WordsData.kt
@@ -33,11 +33,6 @@ val allWords: Set<String> =
         "motorcycle",
         "cauliflower"
     )
-
-/**
- * Maps words to their lengths. Each word in allWords has a unique length. This is required since
- * the words are randomly picked inside GameViewModel and the selection is unpredictable.
- */
 private val wordLengthMap: Map<Int, String> = allWords.associateBy({ it.length }, { it })
 
 internal fun getUnscrambledWord(scrambledWord: String) = wordLengthMap[scrambledWord.length] ?: ""
